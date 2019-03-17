35519ce609169649bedaf7ebd48ec32c5712d8d3
klydd <39828344+klydd@users.noreply.github.com>
2019-03-17 21:49:38 +0300

    About page (#1)
    * Add version
    
    * Add the coolier version
    
    * Remove redundant code
    
    * fix formatting
    
    * update autoformatting
    
    * Update about page, add russian locale
    
    * formatting :+1:

--- app/src/main/java/com/example/chessdetector/MainActivity.kt
+++ app/src/main/java/com/example/chessdetector/MainActivity.kt
@@ -1,19 +1,17 @@
 package com.example.chessdetector
 
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-   
+
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
-import androidx.viewpager.widget.ViewPager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-  
+
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_main.view.*
 import android.provider.MediaStore
@@ -51,7 +49,7 @@ class MainActivity : AppCompatActivity() {
             // start the image capture Intent
             startActivityForResult(intent, 1337)
         }
-  
+
     }
 
 
@@ -67,13 +65,9 @@ class MainActivity : AppCompatActivity() {
         // as you specify a parent activity in AndroidManifest.xml.
         val id = item.itemId
 
-        if (id == R.id.action_settings) {
-            return true
-        }
-
-        if (id == R.id.action_about){
-            setContentView(R.layout.about_main)
-            return true
+        when (id) {
+            R.id.action_settings -> setContentView(R.layout.settings)
+            R.id.action_about -> setContentView(R.layout.about)
         }
 
         return super.onOptionsItemSelected(item)
@@ -88,7 +82,7 @@ class MainActivity : AppCompatActivity() {
             null // returns null if camera is unavailable
         }
     }
-  
+
 
     /**
      * A [FragmentPagerAdapter] that returns a fragment corresponding to
@@ -113,8 +107,10 @@ class MainActivity : AppCompatActivity() {
      */
     class PlaceholderFragment : Fragment() {
 
-        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                                  savedInstanceState: Bundle?): View? {
+        override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
             val rootView = inflater.inflate(R.layout.fragment_main, container, false)
             rootView.section_label.text = getString(R.string.section_format, arguments?.getInt(ARG_SECTION_NUMBER))
             return rootView
